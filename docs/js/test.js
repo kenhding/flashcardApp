9a6d62489d25a2f825a3264023c51bc4b3efcc61
unknown <kkenzo0619@gmail.com>
2023-04-19 00:23:53 +0200

    test

--- docs/js/test.js
+++ docs/js/test.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var injected = document.querySelector('.injected');
+    console.log(injected)
     if (injected) {
       injected.parentNode.removeChild(injected);
     }
